f430e96a7034b6276db8e111393bf24819845a74
Zhihui Yang <youngwisdm@gmail.com>
2025-08-19 16:05:28 +0800

    Enhance boot and loader with page directory and entry point definitions

--- loader/loader.asm
+++ loader/loader.asm
@@ -48,10 +48,10 @@ loader_start:
     or eax, 0x00000001
     mov cr0, eax
 
-    jmp dword SELECTOR_CODE:p_mode_start
+    jmp dword SELECTOR_CODE:protect_mode_start
 
 [bits 32]
-p_mode_start:
+protect_mode_start:
     mov ax, SELECTOR_DATA
     mov ds, ax
     mov es, ax
@@ -60,13 +60,180 @@ p_mode_start:
     mov ax, SELECTOR_VIDEO
     mov gs, ax
 
-    mov byte [gs:0xa0], 'P'
-    mov byte [gs:0xa1], 0x1F    ; 蓝底白字
-    mov byte [gs:0xa2], 'R'
-    ;mov byte [gs:164], 'O'
-    ;mov byte [gs:166], 'T'
-    ;mov byte [gs:168], 'E'
-    ;mov byte [gs:16a], 'C'
-    ;mov byte [gs:16b], 'T'
+    ; load kernel
+    mov eax, KERNEL_START_SECTOR
+    mov ebx, KERNEL_BIN_BASE_ADDR
+    mov ecx, 200 ; sector numbers
 
-    jmp $
+    call rd_disk_m_32
+    
+
+    call setup_page
+    sgdt [gdt_ptr]
+    mov ebx [gdt_ptr + 2]
+    or dword [ebx + 0x18 + 4], 0xc0000000
+    add dword [gdt_ptr + 2], 0xc0000000
+    add esp, 0xc0000000
+
+    mov eax, PAGE_DIR_TABLE_POS
+    mov cr3, eax
+
+    mov eax, cr0
+    or eax, 0x80000000
+    mov cr0, eax
+
+    lgdt [gdt_ptr]
+
+    jmp SELECTOR_CODE:enter_kernel
+enter_kernel:
+    call kernel_init
+    mov esp, 0xc009f000
+    jmp KERNEL_ENTRY_POINT
+
+; read write hard disk
+rd_disk_m_32:
+    mov esi, eax
+    mov di, cx
+
+; set the number of sector
+    mov dx, 0x1f2  ; set the port 0x1f2
+    mov al, cl
+    out dx, al
+
+    mov eax, esi
+; save the LBA addr to 0x1f3 ~ 0x1f6
+    mov dx, 0x1f3
+    out dx, al
+
+    mov cl, 8
+    shr eax, cl
+    mov dx, 0x1f4
+    out dx, al
+
+    shr eax, cl
+    mov dx, 0x1f5
+    out dx, al
+
+    shr eax, cl
+    and al, 0x0f
+    or  al, 0xe0
+    mov dx, 0x1f6
+    out dx, al
+
+; set the read mode: 0x20 to port 0x1f7
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+; check the status
+  .not_ready:
+    nop
+    in al, dx
+    and al, 0x88
+    cmp al, 0x08
+    jnz .not_ready
+
+; read data from port 0x1f0
+    mov ax, di
+    mov dx, 256  ; 256 = di*512/2, each time "in" operator will read 2 byte
+    mul dx
+    mov cx, ax
+    mov dx, 0x1f0
+
+; loop 256 times
+  .go_on_read:
+    in ax, dx
+    mov [bx], ax
+    add bx, 2
+    loop .go_on_read
+    ret
+
+
+
+; create PDE and PTE
+setup_page:
+    mov ecx, 4096
+    mov esi, 0
+.clear_page_dir:
+    mov byte [PAGE_DIR_TABLE_POS + esi], 0
+    inc esi
+    loop .clear_page_dir
+
+; create PDE
+.create_pde:
+    mov eax, PAGE_DIR_TABLE_POS
+    add eax, 0x1000
+    mov ebx, eax
+    or eax, PG_US_U | PG_RW_W | PG_P
+    mov [PAGE_DIR_TABLE_POS + 0x0], eax
+    mov [PAGE_DIR_TABLE_POS + 0xc00], eax
+    sub eax, 0x1000
+    mov [PAGE_DIR_TABLE_POS + 4092], eax
+
+; create PTE
+    mov ecx, 256 ; set the loop times, 1M lowmem / pagesize 4k = 256
+    mov esi, 0
+    mov edx, PG_US_U |  PG_RW_W | PG_P
+.create_pte:
+    mov [ebx+esi*4], edx
+    add edx, 4096
+    inc esi
+    loop .create_pte
+
+; create PDE for kernel
+    mov eax, PAGE_DIR_TABLE_POS
+    add eax, 0x2000
+    or eax, PG_US_U | PG_RW_W | PG_P
+    mov ebx, PAGE_DIR_TABLE_POS
+    mov ecx, 254
+    mov esi, 769
+.create_kernel_pde:
+    mov [ebx+esi*4], eax
+    inc esi
+    add eax, 0x1000
+    loop .create_kernel_pde
+    ret
+
+kernel_init:
+    xor eax, eax
+    xor ebx, ebx
+    xor ecx, ecx
+    xor edx, edx
+
+    mov dx, [KERNEL_BIN_BASE_ADDR + 42]
+    mov ebx, [KERNEL_BIN_BASE_ADDR + 28]
+    add ebx, KERNEL_BIN_BASE_ADDR
+    mov cx, [KERNEL_BIN_BASE_ADDR + 44]
+
+.each_segment:
+    cmp byte [ebx + 0], PT_NULL
+    je .PTNULL
+
+    push dword [ebx + 16]
+    mov eax, [ebx + 4]
+    add eax, KERNEL_BIN_BASE_ADDR
+    push eax
+    push dword [ebx + 8]
+    call mem_cpy
+    add esp, 12
+
+.PTNULL
+    add ebx, edx
+    loop .each_segment
+    ret
+
+mem_cpy:
+    cld
+    push ebp
+    mov ebp, esp
+    push ecx
+
+    mov edi, [ebp + 8]
+    mov esi, [ebp + 12]
+    mov ecx, [ebp + 16]
+    rep movsb
+
+    pop ecx
+    pop ebp
+    ret
+
