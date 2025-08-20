c3d20ec016109d9bd9be517f69538cc45eec530c
Zhihui Yang <youngwisdm@gmail.com>
2025-08-20 15:05:51 +0800

    add print func

---
+++ kernel/main.cpp
@@ -0,0 +1,19 @@
+# include "print/print.h"
+
+void main(void)
+{
+    put_char('k');
+    put_char('e');
+    put_char('r');
+    put_char('n');
+    put_char('e');
+    put_char('l');
+    put_char('\n');
+    put_char(' ');
+    put_char('2');
+    put_char('3');
+    put_char('\n');
+    put_char(' ');
+    put_char('3');
+    while(1);
+}
